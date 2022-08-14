eabde412f2b3c26396f0ea858604572314e90a67
k_henmi <k_henmi@ism.co.jp>
2022-08-14 18:36:21 +0900

    first commit

---
+++ src/resources/views/layouts/app.blade.php
@@ -0,0 +1,11 @@
+<!doctype html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <title>@yield('title')</title>
+</head>
+<body>
+
+</body>
+</html>
